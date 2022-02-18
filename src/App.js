a126a8710391184b70a4c30005491c6650a005f7
TheComeBackGuy <dennis.hart@gmail.com>
2022-02-17 20:11:57 -0500

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
 
-function App() {
+import styled from 'styled-components'
+
+export default function App() {
+
+  const Logo = styled.h1`
+  font-family: 'Oswald', sans-serif;
+    transform-origin: 50% 50%;
+  `
+  function LogoFlip(word) {
+    return <Logo>{word}</Logo>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    {LogoFlip('FLIP')}
       </header>
     </div>
   );
 }
-
-export default App;
